e5c4cf8058430375ed02d0eae4aba989a349e79b
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-13 17:33:49 +0100

    finished home page

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "sqlite3"
+
+DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
+  @artists = DB.execute("SELECT name, id FROM artists").sort
   erb :index
 end
